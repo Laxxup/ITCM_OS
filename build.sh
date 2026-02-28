50d97cede463292e0c1d47761e1f2967c5b0bd77
Laxxup <surezluis4@gmail.com>
2026-02-28 17:52:11 -0600

    ITCM_OS V2: Reparando el sistema base y arranque

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-apt update && apt install debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools -y
-debootstrap --variant=minbase --include=linux-image-amd64,lxde-core,network-manager,sudo,fastfetch,firmware-linux daedalus ./chroot http://deb.devuan.org/merged
-# Aqui van tus configuraciones manuales (Usuario, Locales, Temas)
-# ... (GitHub ejecutara esto desde cero)
+# 1. Instalar herramientas de empaquetado
+sudo apt-get update
+sudo apt-get install -y debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
+
+# 2. Crear un sistema base minimo (Esto es lo que faltaba)
+sudo debootstrap --variant=minbase --include=linux-image-amd64,lxde-core,network-manager,sudo,fastfetch daedalus ./chroot http://deb.devuan.org/merged
+
+# 3. Inyectar tu wallpaper
+sudo mkdir -p ./chroot/usr/share/images/desktop-base/
+sudo cp wallpaperITCMOS.jpg ./chroot/usr/share/images/desktop-base/
+
+# 4. Crear la estructura de la ISO
 mkdir -p image/live
-mksquashfs chroot image/live/filesystem.squashfs -comp xz -e boot
-cp chroot/boot/vmlinuz* image/live/vmlinuz
-cp chroot/boot/initrd* image/live/initrd
+sudo mksquashfs chroot image/live/filesystem.squashfs -comp xz -e boot
+
+# 5. Copiar archivos de arranque
+sudo cp chroot/boot/vmlinuz* image/live/vmlinuz
+sudo cp chroot/boot/initrd* image/live/initrd
+
+# 6. Generar la ISO final
 grub-mkrescue -o ITCM_OS_v1.iso image
